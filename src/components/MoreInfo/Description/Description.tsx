018e96971485d684124d3b9abf4ef9a7173d3d27
varustomyan <v.arustomyan@icloud.com>
2022-08-14 16:34:44 +0300

    feat: added tab description

---
+++ src/components/MoreInfo/Description/Description.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Description.module.css';
+
+function Description() {
+  return (
+    <div className={styles.root}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet debitis dignissimos
+        dolorum eaque eos est et expedita facere illum minima, nemo nobis non nulla odit
+        perferendis, quae quaerat ratione, rem similique sit tempora tempore tenetur unde vel.
+        Accusantium corporis enim et, in molestiae nulla officiis pariatur ratione recusandae
+        voluptatibus.
+      </p>
+    </div>
+  );
+}
+
+export default Description;
--- src/components/MoreInfo/MoreInfo.tsx
+++ src/components/MoreInfo/MoreInfo.tsx
@@ -3,22 +3,32 @@ import styles from './MoreInfo.module.css';
 import UnderlineButton from '../shared/UnderlineButton/UnderlineButton';
 import Characteristics from './Characteristics/Characteristics';
 import { RootState, useAppSelector } from '../../store';
+import Description from './Description/Description';
 
 function MoreInfo() {
-  const [section] = useState('characteristics');
+  const [section, setSection] = useState('characteristics');
 
   const { characteristics } = useAppSelector((state: RootState) => state.product.product);
+
+  const handleDescription = () => {
+    setSection('description');
+  };
+
+  const handleCharacteristics = () => {
+    setSection('characteristics');
+  };
 
   return (
     <div className={styles.root}>
       <div className={styles.nav}>
-        <UnderlineButton>Описание</UnderlineButton>
-        <UnderlineButton>Характеристики</UnderlineButton>
+        <UnderlineButton onClick={handleDescription}>Описание</UnderlineButton>
+        <UnderlineButton onClick={handleCharacteristics}>Характеристики</UnderlineButton>
         <UnderlineButton>Отзывы</UnderlineButton>
       </div>
       {section === 'characteristics' && (
         <Characteristics characteristics={Object.values(characteristics)} />
       )}
+      {section === 'description' && <Description />}
     </div>
   );
 }
